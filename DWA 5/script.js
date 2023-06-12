69b1465efd347e297c7231c6acc59b8b9a302322
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-06-12 14:55:22 +0200

    DWA5,6 and 7 submition

--- DWA 5/script.js
+++ DWA 5/script.js
@@ -6,6 +6,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  
   if (dividend === "" || divider === "") {
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
     return;
