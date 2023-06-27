6ccc4a79d15bc7c92179588a527e35f66f4ade3d
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-06-27 09:12:05 +0200

    DWA 12

--- DWA 11/Script.js
+++ DWA 11/Script.js
@@ -1,69 +1,70 @@
-// Define the actions
-const ADD = 'ADD';
-const SUBTRACT = 'SUBTRACT';
-const RESET = 'RESET';
 
-// Define the reducer function
-function reducer(state = { count: 0 }, action) {
+// Define constants
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 1;
+// Define action types
+const ActionTypes = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+  RESET: "RESET",
+};
+// Define action creators
+function incrementCounter() {
+  return { type: ActionTypes.INCREMENT };
+}
+function decrementCounter() {
+  return { type: ActionTypes.DECREMENT };
+}
+function resetCounter() {
+  return { type: ActionTypes.RESET };
+}
+// Define reducer function
+function counterReducer(state = 0, action) {
   switch (action.type) {
-    case ADD:
-      return { count: state.count + 1 };
-    case SUBTRACT:
-      return { count: state.count - 1 };
-    case RESET:
-      return { count: 0 };
+    case ActionTypes.INCREMENT:
+      return state + STEP_AMOUNT;
+    case ActionTypes.DECREMENT:
+      return state - STEP_AMOUNT;
+    case ActionTypes.RESET:
+      return 0;
     default:
       return state;
   }
 }
-
-// Define the store
-function createStore(reducer) {
-  let state = reducer(undefined, {});
-  const subscriptions = [];
-
-  function getState() {
-    return state;
+// Create a Redux store
+const store = Redux.createStore(counterReducer);
+// Update the UI with the initial state
+let currentValue = store.getState();
+function updateUI() {
+  currentValue = store.getState();
+  number.value = currentValue.toString();
+  if (currentValue <= MIN_NUMBER) {
+    subtract.disabled = true;
+  } else if (currentValue >= MAX_NUMBER) {
+    add.disabled = true;
+  } else {
+    subtract.disabled = false;
+    add.disabled = false;
   }
-
-  function dispatch(action) {
-    state = reducer(state, action);
-    subscriptions.forEach((subscription) => subscription());
-  }
-
-  function subscribe(callback) {
-    subscriptions.push(callback);
-    return function unsubscribe() {
-      const index = subscriptions.indexOf(callback);
-      if (index !== -1) {
-        subscriptions.splice(index, 1);
-      }
-    };
-  }
-
-  return {
-    getState,
-    dispatch,
-    subscribe,
-  };
+  console.log(currentValue);
 }
-
-// Create the store
-const store = createStore(reducer);
-
-// Subscribe to state changes
-const unsubscribe = store.subscribe(() => {
-  console.log('New state:', store.getState());
+// Subscribe to state changes and update the UI
+store.subscribe(updateUI);
+// Attach event listeners to the buttons
+const number = document.querySelector('[data-key="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
+subtract.addEventListener("click", () => {
+  store.dispatch(decrementCounter());
 });
-
-// Initial state
-console.log('Initial state:', store.getState());
-
-// Dispatch actions
-store.dispatch({ type: ADD });
-store.dispatch({ type: ADD });
-store.dispatch({ type: SUBTRACT });
-store.dispatch({ type: RESET });
-
-// Unsubscribe from state changes
-unsubscribe();
+add.addEventListener("click", () => {
+  store.dispatch(incrementCounter());
+});
+const resetButton = document.querySelector("sl-button");
+resetButton.addEventListener("click", () => {
+  store.dispatch(resetCounter());
+  alert("The counter has been reset.");
+});
+// Initialize the UI
+updateUI();
